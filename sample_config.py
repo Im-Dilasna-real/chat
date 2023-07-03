2884d5d71ad545ca2b9b08d45dbae1bc9f783865
imdila <112766883+imdila@users.noreply.github.com>
2023-07-03 09:39:56 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,5 @@
 owner_id = 2066763897
-bot_token = "2111353168:AAGMUnFi8LM_ObHGK21oc8rUfNZXXevXI-c"
+bot_token = 2111353168:AAGMUnFi8LM_ObHGK21oc8rUfNZXXevXI-c
 bot_id = 2111353168
 
 # Leave it as it is
