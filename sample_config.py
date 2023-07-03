68641e404fa8e9ddb8ba2cf584b898b462653739
imdila <112766883+imdila@users.noreply.github.com>
2023-07-03 09:37:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,4 +3,4 @@ bot_token = "2111353168:AAGMUnFi8LM_ObHGK21oc8rUfNZXXevXI-c"
 bot_id = 2111353168
 
 # Leave it as it is
-ARQ_API_BASE_URL = "https://thearq.tech"
+ARQ_API_BASE_URL = "https://grambuilders.tech"
